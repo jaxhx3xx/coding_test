c7794fbbd40b4547ab732a00cdaca5da84ea6db4
정재희 <jaehuij660@gmail.com>
2025-07-14 10:55:41 +0900

    [level 0] Title: 합성수 찾기, Time: 0.26 ms, Memory: 87.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int count = 0;
+        int count1 = 0;
+        for(int i=1; i<=n; i++){
+            count = 0;
+            for(int j =1; j<=n; j++){
+                if(i%j==0) count++;
+            }
+            if(count>=3) count1++;
+        }
+        return count1;
+    }
+}
