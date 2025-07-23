d9bfc04da3cc0eb9bea9a5d3b0ab2ab5f044eeaf
정재희 <jaehuij660@gmail.com>
2025-07-23 23:22:52 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.c
@@ -0,0 +1,14 @@
+#define CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void){
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b);
+}
