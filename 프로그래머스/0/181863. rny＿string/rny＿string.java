6e6cdfa5c20af88f4ab9c52e68a1337ba2ef55f9
정재희 <jaehuij660@gmail.com>
2026-05-27 09:26:54 +0900

    [level 0] Title: rny_string, Time: 0.03 ms, Memory: 61.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        if(rny_string.contains("m")==true) answer = rny_string.replace("m","rn");
+        else answer = rny_string;
+        return answer;
+    }
+}
