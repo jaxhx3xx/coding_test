278cc8551983e04f7afae22f847bf0c9b1792995
정재희 <jaehuij660@gmail.com>
2026-05-28 11:17:17 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 1.15 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(my_string);
+        answer += sb.replace(s,s+overwrite_string.length(),overwrite_string);
+        return answer;
+    }
+}
