65664ad0f27c823aa074181a4c7a054b89edc680
정재희 <jaehuij660@gmail.com>
2026-07-19 22:15:27 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.02 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        if(a%2!=0 && b%2!=0){
+            answer += (a*a)+(b*b);
+        }
+        else if(a%2!=0 && b%2==0){
+            answer+= (a+b)*2;
+        }
+        else if(a%2==0 && b%2!=0){
+            answer+= (a+b)*2;
+        }
+        else{
+            answer+= a-b;
+            if(answer <0){
+                answer*=-1;
+            }
+        }
+        return answer;
+    }
+}
